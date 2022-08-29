d64e33004542a8a2140898a62ae73a4bf6e006bd
Louise McCafferty <mccafferty_@hotmail.com>
2022-08-29 16:40:56 +0100

    initial commit

---
+++ precourse_recap.py
@@ -0,0 +1,16 @@
+first_name = "louise"
+second_name = "mccafferty"
+full_name = first_name + second_name
+
+print(full_name.capitalize())
+
+number1 = 3
+number2 = "6"
+number3 = 1
+
+print(3 + 1)
+print(2 * 2)
+print(int(number2) + number1)
+print(int(number2) / number3)
+
+
